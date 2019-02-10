75ecf011e6b8c345cbb4e8bcbb11aa72e99ef249
allisbri <allisbri@oregonstate.edu>
2019-02-10 02:40:13 -0500

    debugging recyclerview

--- app/src/main/java/com/mobilepgh/movie_smoothie/MainActivity.java
+++ app/src/main/java/com/mobilepgh/movie_smoothie/MainActivity.java
@@ -2,20 +2,40 @@ package com.mobilepgh.movie_smoothie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MovieAdapter movieAdapter = new MovieAdapter();
-    movieAdapter = new MovieAdapter();
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<String> urls = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+        for (int i = 0; i < 10; i++) {
+            urls.add("http://i.imgur.com/DvpvklR.png");
+        }
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.rv_posters);
+        MovieAdapter movieAdapter = new MovieAdapter(urls, this);
+        recyclerView.setAdapter(movieAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
     }
 
 
--- app/src/main/java/com/mobilepgh/movie_smoothie/MovieAdapter.java
+++ app/src/main/java/com/mobilepgh/movie_smoothie/MovieAdapter.java
@@ -1,8 +1,69 @@
 package com.mobilepgh.movie_smoothie;
 
-public class MovieAdapter {
-    private int test;
-    public MovieAdapter(){
-        test = 1;
+import android.content.Context;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    private static final String TAG = "MovieAdapter";
+
+    private ArrayList<String> mPosterUrls = new ArrayList<>();
+    private Context mContext;
+
+    public MovieAdapter(ArrayList<String> mPosters, Context mContext) {
+        this.mPosterUrls = mPosters;
+        this.mContext = mContext;
+    }
+
+    //place view holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.poster_grid_item,
+                viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    //insert data in viewholder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
+        Log.d(TAG, "onBindViewHolder: called.");
+        Picasso
+                .get()
+                .load(mPosterUrls.get(i))
+                .into(viewHolder.poster);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosterUrls.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView poster;
+        LinearLayout parent;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            poster = itemView.findViewById(R.id.iv_item_number);
+            parent = itemView.findViewById(R.id.ll_item_parent);
+
+        }
+
     }
 }
